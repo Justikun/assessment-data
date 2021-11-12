30575fe335f5d9c9f1746d59182c2fc9e0b025b6
Justin Lowry <github@lowry.life>
2021-11-12 09:28:12 -0700

    first commit

---
+++ process.py
@@ -0,0 +1,12 @@
+# opening specified file
+log_file = open("um-server-01.txt")
+
+#function print out the entire lines for all the 'Mon' days
+def sales_reports(log_file):    # defining a function called called_report
+    for line in log_file:       # Looping through each line in the open file
+        line = line.rstrip()    # removing the \n
+        day = line[0:3]         # Setting the first 3 chars in open file to day
+        if day == "Mon":        # If day is equal to 'Mon' print something
+            print(line)         # print the line to the console.
+
+sales_reports(log_file)         # calling the function with specified file
